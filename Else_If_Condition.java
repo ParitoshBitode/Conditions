80947b5b332693e02617c2c9a0c7360f2e4680b0
ParitoshBitode <paritosh.bitode@gmail.com>
2024-01-30 00:48:49 +0530

    else if statement

---
+++ Else_If_Condition.java
@@ -0,0 +1,67 @@
+package control_Statement;
+
+import java.util.Scanner;
+
+public class Else_If_Condition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		char ch;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the character :");
+		ch = sc.next().charAt(0);
+		
+		if(ch == 'a')
+		{
+			System.out.println("Its a Vowel");
+		}
+		else if(ch == 'A')
+		{
+			System.out.println("Its a Vowel");
+		}
+		else if(ch == 'e')
+		{
+			System.out.println("Its a Vowel");
+		}
+		else if(ch == 'E')
+		{
+			System.out.println("Its a Vowel");
+		}
+		else if(ch == 'i')
+		{
+			System.out.println("Its a Vowel");
+		}
+		else if(ch == 'o')
+		{
+			System.out.println("Its a Vowel");
+		}
+		else if(ch == 'O')
+		{
+			System.out.println("Its a Vowel");
+		}
+		else if(ch == 'u')
+		{
+			System.out.println("Its a Vowel");
+		}
+		else if(ch == 'U')
+		{
+			System.out.println("Its a Vowel");
+		}
+		else
+		{
+			System.out.println("It not a vowel");
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
